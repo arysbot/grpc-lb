0738434dcdaeff79d1b1d0d832c2ed5536f99480
torsina <theo.sagot.1@gmail.com>
2018-11-13 00:32:27 +0100

    remove server code from client branch

--- src/index.js
+++ src/index.js
@@ -4,18 +4,7 @@ const os = require('os');
 
 const protoPath = path.join(__dirname, 'testing.proto');
 const proto = grpc.load(protoPath).testing;
-const isServer = (process.env.RPC_TYPE === 'server');
-
-let server;
 let client;
-
-function serverDoStuff(ctx, callback) {
-    console.log(`Processing "doStuff" from "${ctx.request.host}"...`);
-    callback(null, {
-        message: `Hello, ${ctx.request.name}; hold on while I do things!`,
-        host: os.hostname()
-    });
-}
 
 function clientDoStuff() {
     const dataToSend = {
@@ -33,24 +22,10 @@ function clientDoStuff() {
 
     setTimeout(clientDoStuff, 2000);
 }
-console.log(process.env.RPC_TYPE );
-if (Boolean(isServer)) {
+// client
+client = new proto.Stuff(`dns:///${process.env.RPC_HOST}:${process.env.RPC_PORT}`, grpc.credentials.createInsecure(), {
+    'grpc.lb_policy_name': 'round_robin'
+});
 
-    // server
-    server = new grpc.Server();
-    server.addService(proto.Stuff.service, {doStuff: serverDoStuff});
-    server.bind(`0.0.0.0:${process.env.RPC_PORT}`, grpc.ServerCredentials.createInsecure());
-    server.start();
-
-    console.log('Listening in server mode...');
-
-} else {
-
-    // client
-    client = new proto.Stuff(`dns:///${process.env.RPC_HOST}:${process.env.RPC_PORT}`, grpc.credentials.createInsecure(), {
-        'grpc.lb_policy_name': 'round_robin'
-    });
-
-    // begin doing stuff
-    clientDoStuff();
-}
+// begin doing stuff
+clientDoStuff();
