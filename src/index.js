7c3d66ba214a8b363230b90aaceca27372e602c2
torsina <theo.sagot.1@gmail.com>
2018-11-22 08:31:57 +0100

    update server to arys test env

--- src/index.js
+++ src/index.js
@@ -2,22 +2,19 @@ const grpc = require('grpc');
 const path = require('path');
 const os = require('os');
 
-const protoPath = path.join(__dirname, 'testing.proto');
-const proto = grpc.load(protoPath).testing;
+const protoPath = path.join(__dirname, '../node_modules/protofiles/src/Logger.proto');
+const proto = grpc.load(protoPath);
 
 let server;
 
 function serverDoStuff(ctx, callback) {
-    console.log(`Processing "doStuff" from "${ctx.request.host}"...`);
-    callback(null, {
-        message: `Hello, ${ctx.request.name}; hold on while I do things!`,
-        host: os.hostname()
-    });
+    console.log(ctx.request);
+    callback(null, {});
 }
 
 server = new grpc.Server();
-server.addService(proto.Stuff.service, {doStuff: serverDoStuff});
-server.bind(`0.0.0.0:${process.env.RPC_PORT}`, grpc.ServerCredentials.createInsecure());
+server.addService(proto.Logger.service, {Log: serverDoStuff});
+server.bind(`127.0.0.1:8881`, grpc.ServerCredentials.createInsecure());
 server.start();
 
 console.log('Listening in server mode...');
